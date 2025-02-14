bb5172583cb87dec621ad130e95c1c4c57f0d1ae
Andrew Suda <asud9927@stu.gusd.net>
2025-02-14 19:07:14 +0000

    templet course page development

--- course-navigator/src/CourseDescription.js
+++ course-navigator/src/CourseDescription.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. HELLO
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import styles from './CourseDescription.css';
+function CourseDescription(){  
+    return (
+      <body>
+        <img className = "falcon "src = "https://jacobpoole.net/tfalcon.png" alt = "React Image"/>
+        <img className = "person" src = "https://www.freeiconspng.com/uploads/clipart--person-icon--cliparts-15.png" alt = "React Image 2"/>
+        <button className = "logIn">
+              Log In
+          </button>
+          <button className = "signUp">
+              Sign Up 
+          </button>
+        <div className = "line"></div>
+      <div className = "head">
+        <h1>Honors Software Development Internship</h1>
+        <div className = "rectangle">
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed purus quis eros sodales semper. Aenean interdum enim eget vulputate maximus. Vivamus sem mi, sollicitudin ac lectus quis, hendrerit feugiat lacus. In hac habitasse platea dictumst. Praesent ac magna eu purus tristique finibus ac ultricies nibh. Aliquam quis tellus sollicitudin, iaculis nulla ut, convallis enim. Duis commodo laoreet libero a pretium. Vestibulum sed purus congue, tincidunt purus vel, ultrices libero. Sed vitae dui in dolor faucibus fringilla interdum sit amet turpis. Vestibulum pretium nulla quis tincidunt lacinia.
+            </p>
+        </div>
+      </div>
+      </body>
+      
+            
+          
+        
+      
+      
+    );
 }
-
-export default App;
+export default CourseDescription;
