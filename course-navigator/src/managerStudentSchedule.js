bf72d2067900f7945aad9b7095f560d8ce829662
Loduan10 <133896343+Loduan10@users.noreply.github.com>
2025-02-15 02:07:37 +0000

    managerSchedule page updates

--- course-navigator/src/managerStudentSchedule.js
+++ course-navigator/src/managerStudentSchedule.js
@@ -10,29 +10,32 @@ function Manager() {
       <div class = "topnav">
         <a class = "active" href="#Home">Home </a>
         <a href="#Course Description">Course Description </a>
-        <a href="#Classes ">Student Schedules</a>
+        <a href="#Classes ">Student Information</a>
         <input type = "text" placeholder = "Search.."/>
       </div>
-      <div class="row">
-      <div class="leftcolumn">
-      <h2>TITLE HEADING</h2>
-      <p>Some text..</p>
-    </div>
-    <div class="rightcolumn">
-      <h2>TITLE HEADING</h2>
-      <p>Some text..</p>
-    </div>
-      <div class="column">
-        <button>"Example Button"</button>
-      </div>
-      <div class="column">
-        <button>"Example Button"</button>
-      </div>
-      <div class="column">
-        <button>"Example Button"</button>
-      </div>
+      <button class="tablink" onclick="openPage('Student Information ', this, 'red')">Student Information</button>
+      <button class="tablink" onclick="openPage('Student Transcripts', this, 'green')" id="defaultOpen">Student Transcripts</button>
+      <button class="tablink" onclick="openPage('Course Requests', this, 'blue')">Course Requests</button>
 
-      </div>
+<div id="Home" class="tabcontent">
+  <h3>Home</h3>
+  <p>Home is where the heart is..</p>
+</div>
+
+<div id="News" class="tabcontent">
+  <h3>News</h3>
+  <p>Some news this fine day!</p>
+</div>
+
+<div id="Contact" class="tabcontent">
+  <h3>Contact</h3>
+  <p>Get in touch, or swing by for a cup of coffee.</p>
+</div>
+
+<div id="About" class="tabcontent">
+  <h3>About</h3>
+  <p>Who we are and what we do.</p>
+</div>
       {/*
       This is JS as well
       */}
