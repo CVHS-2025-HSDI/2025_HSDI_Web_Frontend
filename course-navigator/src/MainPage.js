2f310bf55da0f13d79dcfe950871e1c6f8ff4b34
Cristiann-py <cristiannhudsonn@gmail.com>
2025-02-19 11:03:25 -0800

    Progress

--- course-navigator/src/MainPage.js
+++ course-navigator/src/MainPage.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import './MainPage.css';
 
-function App() {
+function MainPage() {
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSlideIndex((prevIndex) => (prevIndex + 1) % 3); // Cycle through 3 slides
+    }, 2000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. HELLO
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Cristian
-        </a>
-      </header>
+    <div className="MainPage">
+        <img src="./logo.png" className="image3" />
+      <div className="container">
+      <div className = "header">
+          
+          <h1>Crescenta Valley High School</h1>
+      </div>
+      <button class = "Signinbutton">Sign In</button>
+      <img src="./profileimage.png" className="profileimage"/>
+      <div className="paragraphtext">
+    <p>🎉✨ Welcome to Course Navigator! ✨🎉 🚀 Your school is full of amazing opportunities, and we’re here to help you find them! Whether you want to learn new things 📚, join exciting clubs 🎭⚽, or meet awesome friends, we’ve got you covered! 💡 Plus, real students share their experiences so you know exactly what to expect! 🤩 Never wonder: ❓ Is this class fun? ❓ Is this club worth it? ❓ Will I make new friends? We bring you real answers from real students! 🌟🏆 Start exploring now and make this your best school year ever! 🚀🎉</p>
+      </div>
+
+
+        {/* Background Image */}
+        <img src="./Background.png" className="background" alt="Background" />
+        {/* Slideshow Container */}
+        <div className="slideshow-container">
+          
+          {[1, 2, 3].map((num, index) => (
+            <div
+              key={index}
+              className={`mySlides ${slideIndex === index ? "fade active-slide" : ""}`}
+            >
+              <img src={`img${num}.png`} alt={`Slide ${num}`} style={{ width: '100%', height: '170%'}} />
+            </div>
+          ))}
+        </div>
+
+        {/* Hump Image */}
+        <img src="./hump.png" className="image2" alt="Hump" />
+      </div>
+
+      <script>
+        
+      </script>
     </div>
   );
 }
 
-export default App;
+export default MainPage;
