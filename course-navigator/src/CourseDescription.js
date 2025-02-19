33e9db9f45980cacfc7ecf014aec2ebcc0bf1d6b
Andrew Suda <asud9927@stu.gusd.net>
2025-02-19 19:28:34 +0000

    made page reactive exit

--- course-navigator/src/CourseDescription.js
+++ course-navigator/src/CourseDescription.js
@@ -2,19 +2,21 @@ import styles from './CourseDescription.css';
 function CourseDescription(){  
     return (
       <body>
-        <img className = "falcon "src = "https://jacobpoole.net/tfalcon.png" alt = "React Image"/>
-        <img className = "person" src = "https://www.freeiconspng.com/uploads/clipart--person-icon--cliparts-15.png" alt = "React Image 2"/>
-        <button className = "logIn">
-              Log In
-          </button>
-          <button className = "signUp">
-              Sign Up 
-          </button>
-        <div className = "line"></div>
+          <img className = "falcon "src = "https://jacobpoole.net/tfalcon.png" alt = "React Image"/>
+          <img className = "person" src = "https://www.freeiconspng.com/uploads/clipart--person-icon--cliparts-15.png" alt = "React Image 2"/>
+          <button className = "logIn">
+                Log In
+            </button>
+            <button className = "signUp">
+                Sign Up 
+            </button> 
+            <div>
+          <div className = "line"></div>
+          </div>
       <div className = "head">
-        <h1>Honors Software Development Internship</h1>
+        <h1 className = "title" >Honors Software Development Internship</h1>
         <div className = "rectangle">
-            <p>
+            <p className = "description">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed purus quis eros sodales semper. Aenean interdum enim eget vulputate maximus. Vivamus sem mi, sollicitudin ac lectus quis, hendrerit feugiat lacus. In hac habitasse platea dictumst. Praesent ac magna eu purus tristique finibus ac ultricies nibh. Aliquam quis tellus sollicitudin, iaculis nulla ut, convallis enim. Duis commodo laoreet libero a pretium. Vestibulum sed purus congue, tincidunt purus vel, ultrices libero. Sed vitae dui in dolor faucibus fringilla interdum sit amet turpis. Vestibulum pretium nulla quis tincidunt lacinia.
             </p>
         </div>
