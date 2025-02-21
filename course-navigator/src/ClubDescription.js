286832133dfe11abda99709ccdf1cc4684b9a072
tilo78 <111925781+tilo78@users.noreply.github.com>
2025-02-21 19:35:06 +0000

    almost done?

--- course-navigator/src/ClubDescription.js
+++ course-navigator/src/ClubDescription.js
@@ -1,8 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import emailIcon from './emailIcon.png';
 import instaIcon from './instaIcon.png';
 // import remindIcon from './remindIcon.png';
 import locationIcon from './locationIcon.png';
+import image from './image.png';
+import falcon from './falcon.png';
+import accountIcon from './accountIcon.png';
 
 
 import './ClubDescription.css';
@@ -14,10 +17,7 @@ function ClubDescription() {
     <div className="ClubDescription">
       <div className="nav">
         <a href="#home">
-          <img src={logo} className="ClubDescription-logo" />
-          </a>
-        <a href="#account">
-          <img src={logo} className="account-logo" />
+          <img src={falcon} className="falcon" alt="falcon" />
           </a>
         <a href="#login">
           <button className="account-buttons">Log In</button>
@@ -25,48 +25,50 @@ function ClubDescription() {
         <a href="#signup">
           <button className="account-buttons">Sign Up</button>
           </a>
+        <a href="#account">
+          <img src={accountIcon} className="accountIcon" alt="accountIcon" />
+          </a>
+        </div>
+        
+
+      <div className="flex ClubDescription-body">
+        <div className="col pics">
+          <img src={image} className="club-pic" alt="club-pic" />
+          <img src={image} className="club-pic" alt="club-pic" />
         </div>
 
-      <div className="ClubDescription-body">
-        <div className="flex">
-          <div className="col pics">
-            <img src={instaIcon} className="club-pic" />
-            <img src={instaIcon} className="club-pic" />
+        <div className="col description">
+          <h2>Dance</h2>
+          <p className="descbox">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tempor velit feugiat dolor finibus, a iaculis eros volutpat. Vivamus urna dolor, hendrerit non enim vel, rhoncus imperdiet elit. Etiam placerat magna at nunc sollicitudin, eget rhoncus ipsum scelerisque. Nunc convallis ante non ipsum porta volutpat. Mauris ultrices tellus in risus accumsan, nec tincidunt sapien placerat. </p>
+        </div>
+
+        <div className="col contacts">
+          <div className="flex">
+            <img src={image} className="officer-pic" alt="officer-pic" />
+            <p className="center">President's name (President)</p>
           </div>
-          <div className="col description">
-            <h2>Dance</h2>
-            <p className="descbox">sssssssssssssssssssssss ssssssssssssssss sssssssss sssssssssssssssssssssssssssssss</p>
+          <div className="flex">
+            <img src={image} className="officer-pic" alt="officer-pic" />
+            <p className="center">VP's name (Vice President)</p>
           </div>
-          <div className="col contacts">
-            <div className="flex">
-              <img src={logo} className="officer-pic" />
-              <p className="center">President's name (President)</p>
 
-            </div>
-            <div className="flex">
-              <img src={logo} className="officer-pic" />
-              <p className="center">VP's name (Vice President)</p>
+          <h5 className="horizontally-centered">Contact Us!</h5>
 
-            </div>
-
-            <h5 className="horizontally-centered">Contact Us!</h5>
-
-            <div className="flex contact">
-              <img src={emailIcon} className="contacts-pic" />
-              <p className="center">exampleemail@gmail.com</p>
-            </div>
-            <div className="flex contact">
-              <img src={instaIcon} className="contacts-pic" />
-              <p className="center">exampleemail@gmail.com</p>
-            </div>
-            <div className="flex contact">
-              <img src={instaIcon} className="contacts-pic" />
-              <p className="center">Join our Remind</p>
-            </div>
-            <div className="flex contact">
-              <img src={locationIcon} className="contacts-pic" />
-              <p className="center">Room xyz</p>
-            </div>
+          <div className="flex contact">
+            <img src={emailIcon} className="contacts-pic" alt="contacts-pic" />
+            <p className="center">exampleemail@gmail.com</p>
+          </div>
+          <div className="flex contact">
+            <img src={instaIcon} className="contacts-pic" alt="contacts-pic" />
+          <p className="center">exampleemail@gmail.com</p>
+          </div>
+          <div className="flex contact">
+            <img src={instaIcon} className="contacts-pic" alt="contacts-pic" />
+            <p className="center">Join our Remind</p>
+          </div>
+          <div className="flex contact">
+            <img src={locationIcon} className="contacts-pic" alt="contacts-pic" />
+            <p className="center">Room xyz</p>
           </div>
         </div>
 
