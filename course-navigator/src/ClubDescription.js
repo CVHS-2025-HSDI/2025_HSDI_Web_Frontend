8e58f017438679c5b54a686e7fb32bcb855535f6
tilo78 <111925781+tilo78@users.noreply.github.com>
2025-02-26 19:39:08 +0000

    changed pic

--- course-navigator/src/ClubDescription.js
+++ course-navigator/src/ClubDescription.js
@@ -1,15 +1,24 @@
 // import logo from './logo.svg';
 import emailIcon from './emailIcon.png';
 import instaIcon from './instaIcon.png';
-import remindIcon from './remindIcon.png';
+import remindIcon from './remindIcon.svg';
 import locationIcon from './locationIcon.png';
-import image from './image.png';
-import falcon from './falcon.png';
+import clubImage1 from './image.png';
+import clubImage2 from './image.png';
+import falconLogo from './falcon.png';
 import accountIcon from './accountIcon.png';
-
 
 import './ClubDescription.css';
 import React from 'react';
+
+var clubName = "Club Name";
+var presName = "Pres Name";
+var vpName = "Vp Name";
+var desc = "all the description text";
+var email = "email@gmail.com";
+var insta = "@insta";
+var remind = "remind";
+var location = "Room 0000";
 
 
 function ClubDescription() {
@@ -17,7 +26,7 @@ function ClubDescription() {
     <div className="ClubDescription">
       <div className="nav">
         <a href="#home">
-          <img src={falcon} className="falcon" alt="falcon" />
+          <img src={falconLogo} className="falcon" alt="falcon" />
           </a>
         <a href="#login">
           <button className="account-buttons">Log In</button>
@@ -33,44 +42,44 @@ function ClubDescription() {
 
       <div className="flex ClubDescription-body">
         <div className="col pics">
-          <img src={image} className="club-pic" alt="club-pic" />
-          <img src={image} className="club-pic" alt="club-pic" />
+          <img src={clubImage1} className="club-pic" alt="club-pic" />
+          <img src={clubImage2} className="club-pic" alt="club-pic" />
         </div>
 
         <div className="col description">
-          <h2>Dance</h2>
+          <h2>{clubName}</h2>
           <div className="descbox">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tempor velit feugiat dolor finibus, a iaculis eros volutpat. Vivamus urna dolor, hendrerit non enim vel, rhoncus imperdiet elit. Etiam placerat magna at nunc sollicitudin, eget rhoncus ipsum scelerisque. Nunc convallis ante non ipsum porta volutpat. Mauris ultrices tellus in risus accumsan, nec tincidunt sapien placerat. </p>
+            <p>{desc}</p>
           </div>
         </div>
 
         <div className="col contacts">
           <div className="flex">
             <img src={accountIcon} className="officer-pic" alt="officer-pic" />
-            <p className="align-left">President's name (President)</p>
+            <p className="align-left">{presName} (President)</p>
           </div>
           <div className="flex">
             <img src={accountIcon} className="officer-pic" alt="officer-pic" />
-            <p className="align-left">VP's name (Vice President)</p>
+            <p className="align-left">{vpName} (Vice President)</p>
           </div>
 
           <h5 className="horizontally-centered">Contact Us!</h5>
 
           <div className="flex contact">
             <img src={emailIcon} className="contacts-pic" alt="contacts-pic" />
-            <p className="align-left">exampleemail@gmail.com</p>
+            <p className="align-left">{email}</p>
           </div>
           <div className="flex contact">
             <img src={instaIcon} className="contacts-pic" alt="contacts-pic" />
-          <p className="align-left">@whatever</p>
+          <p className="align-left">{insta}</p>
           </div>
           <div className="flex contact">
             <img src={remindIcon} className="contacts-pic" alt="contacts-pic" />
-            <p className="align-left">Join our Remind</p>
+            <p className="align-left">{remind} Join our Remind</p>
           </div>
           <div className="flex contact">
             <img src={locationIcon} className="contacts-pic" alt="contacts-pic" />
-            <p className="align-left">Room 0000</p>
+            <p className="align-left">{location}</p>
           </div>
         </div>
 
