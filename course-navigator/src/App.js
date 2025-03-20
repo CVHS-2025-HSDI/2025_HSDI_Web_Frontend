034ca153f62a97b89a000e52e2037061b1db83e0
tatiros11 <tatianarosalyn@gmail.com>
2025-03-20 17:53:48 +0000

    Club Page

--- course-navigator/src/App.js
+++ course-navigator/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Course() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. HELLO
+          Edit <code>src/App.js</code> and save to reload. HELLO T & M
         </p>
         <a
           className="App-link"
