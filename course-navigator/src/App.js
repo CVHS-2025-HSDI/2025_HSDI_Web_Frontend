b14b12b4799c9c71367233881616306c840a8a59
Andrew Suda <asud9927@stu.gusd.net>
2025-04-08 18:06:23 +0000

    Added login page

--- course-navigator/src/App.js
+++ course-navigator/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import GoogleSignIn from './GoogleLogin/GoogleSignIn';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="input">
+        <h2 className="username">Log In</h2>
+        <textarea className="userInput" placeholder="Username"></textarea>
+        <h2></h2>
+        <textarea className="passinput" placeholder="Password"></textarea>
+        <h2></h2>
+        <button className="login">Log In</button>
+      </div>
       <GoogleSignIn /> 
     </div>
   );
