c689360613acee686a693fe23af4f87aba555824
Andrew Suda <asud9927@stu.gusd.net>
2025-04-25 18:36:34 +0000

    login changes

--- course-navigator/src/App.js
+++ course-navigator/src/App.js
@@ -4,17 +4,19 @@ import './App.css';
 
 function App() {
   return (
+    <><img className="falcon " src="https://jacobpoole.net/tfalcon.png" alt="React Image"/>
+      <div className="line"></div>
     <div className="App">
-      <div className="input">
-        <h2 className="username">Log In</h2>
-        <textarea className="userInput" placeholder="Username"></textarea>
-        <h2></h2>
-        <textarea className="passinput" placeholder="Password"></textarea>
-        <h2></h2>
-        <button className="login">Log In</button>
-      </div>
-      <GoogleSignIn /> 
-    </div>
+        <div className="input">
+          <h2 className="title">Log In</h2>
+          <textarea className="userInput" placeholder="Username"></textarea>
+          <h2></h2>
+          <textarea className="passinput" placeholder="Password"></textarea>
+          <h2></h2>
+          <button className="login">Log In</button>
+        </div>
+        <GoogleSignIn />
+      </div></>
   );
 }
 
