77d2cb9901d9ec5b225dc7edf722c5af22f7cfab
Luke Yen <luksogami@icloud.com>
2025-04-28 12:45:42 -0700

    more login backend

--- course-navigator/src/App.js
+++ course-navigator/src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 import axios from 'axios'
 import { useState } from 'react';
 function App(e) {
-  const handleSubmit = async() => {
-    // e.preventDefault();
-    const response = await axios.post('http://localhost:3200/Login', { username, password });
-    console.log(username, password)
-  }
+  
   const [username, setUsername] = useState('blah');
   const [password, setPassword] = useState('bleh');
+  console.log("username:" +username)
+  const handleSubmit = async(e) => {
+    // e.preventDefault();
+    const response = await axios.post('http://localhost:3200/Login', JSON.stringify({ username, password }));
+    console.log(username, password)
+  }
   return (
     <form onSubmit={handleSubmit}>
 
@@ -20,9 +22,9 @@ function App(e) {
     <div className="App">
         <div className="input">
           <h2 className="title">Log In</h2>
-          <textarea className="userInput" type = 'username' placeholder="Username" value= {username} onChange={(e) => setUsername(e.target.value)}></textarea>
+          <textarea className="userInput" type = {username} placeholder="Username" value= {username} onChange={(e) => setUsername(e.target.value)}></textarea>
           <h2></h2>
-          <textarea className="passinput" type = 'password' placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}></textarea>
+          <textarea className="passinput" type = {password} placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}></textarea>
           <h2></h2>
           <button type= 'submit' className="login" onClick={(e) => handleSubmit(e)}>Log In</button>
         </div>
