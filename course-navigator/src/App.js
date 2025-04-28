3ff3a54315968e4913f972239ef30a8be6d3bcfa
Luke Yen <luksogami@icloud.com>
2025-04-28 12:45:31 -0700

    login with backend Co-authored-by: Liam R Zadoorian <XC4Life101@users.noreply.github.com>

--- course-navigator/src/App.js
+++ course-navigator/src/App.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import GoogleSignIn from './GoogleLogin/GoogleSignIn';
 import './App.css';
-
-function App() {
+import axios from 'axios'
+import { useState } from 'react';
+function App(e) {
+  const handleSubmit = async() => {
+    // e.preventDefault();
+    const response = await axios.post('http://localhost:3200/Login', { username, password });
+    console.log(username, password)
+  }
+  const [username, setUsername] = useState('blah');
+  const [password, setPassword] = useState('bleh');
   return (
+    <form onSubmit={handleSubmit}>
+
+   
     <><img className="falcon " src="https://jacobpoole.net/tfalcon.png" alt="React Image"/>
       <div className="line"></div>
     <div className="App">
         <div className="input">
           <h2 className="title">Log In</h2>
-          <textarea className="userInput" placeholder="Username"></textarea>
+          <textarea className="userInput" type = 'username' placeholder="Username" value= {username} onChange={(e) => setUsername(e.target.value)}></textarea>
           <h2></h2>
-          <textarea className="passinput" placeholder="Password"></textarea>
+          <textarea className="passinput" type = 'password' placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}></textarea>
           <h2></h2>
-          <button className="login">Log In</button>
+          <button type= 'submit' className="login" onClick={(e) => handleSubmit(e)}>Log In</button>
         </div>
-        <GoogleSignIn />
+        {/* <GoogleSignIn /> */}
       </div></>
+    </form>
   );
 }
 
