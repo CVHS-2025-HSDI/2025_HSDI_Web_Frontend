ef03a8ab99d783f10f88f6f9dbb0297b471f4321
Luke Yen <luksogami@icloud.com>
2025-04-30 11:11:02 -0700

    input chagn e

--- course-navigator/src/App.js
+++ course-navigator/src/App.js
@@ -5,8 +5,10 @@ import axios from 'axios'
 import { useState } from 'react';
 function App(e) {
   
-  const [username, setUsername] = useState('blah');
-  const [password, setPassword] = useState('bleh');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [response, setResponse] = useState('')
+  const [maskedPassword, setMaskedPassword] = useState('')
   const handleSubmit = async(e) => {
     e.preventDefault();
   
@@ -15,25 +17,29 @@ function App(e) {
       passwd: password
     }
     console.log(data)
-    const response = await axios.post('http://localhost:3200/Login', data);
-    console.log(response.data)
+    const response = await axios.post('http://localhost:3200/Login', data).then(function(response){
+      console.log(response.data)
+      setResponse(response.data)
+    });
   }
   return (
     <form onSubmit={handleSubmit}>
-
-   
     <><img className="falcon " src="https://jacobpoole.net/tfalcon.png" alt="React Image"/>
       <div className="line"></div>
     <div className="App">
         <div className="input">
           <h2 className="title">Log In</h2>
-          <textarea className="userInput" type = {username} name="username" placeholder="Username" value= {username} onChange={(e) => setUsername(e.target.value)}></textarea>
+          <input className="userInput" type = {username} name="username" placeholder="Username" value= {username} onChange={(e) => setUsername(e.target.value)}></input>
           <h2></h2>
-          <textarea className="passinput" type = {password} name="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}></textarea>
+          <input className="passinput" type = "password" name="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
           <h2></h2>
           <button type= 'submit' className="login" onClick={(e)=> handleSubmit(e)}>Log In</button>
+          
         </div>
-        {/* <GoogleSignIn /> */}
+        <text className = 'message'>
+            {response}
+        </text>
+        <GoogleSignIn />
       </div></>
     </form>
   );
