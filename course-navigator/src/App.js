23df19c967d6c4def252a4598018234010171534
Luke Yen <luksogami@icloud.com>
2025-04-29 19:26:58 -0700

    login progress

--- course-navigator/src/App.js
+++ course-navigator/src/App.js
@@ -7,11 +7,16 @@ function App(e) {
   
   const [username, setUsername] = useState('blah');
   const [password, setPassword] = useState('bleh');
-  console.log("username:" +username)
   const handleSubmit = async(e) => {
-    // e.preventDefault();
-    const response = await axios.post('http://localhost:3200/Login', JSON.stringify({ username, password }));
-    console.log(username, password)
+    e.preventDefault();
+  
+    const data = {
+      user: username, 
+      passwd: password
+    }
+    console.log(data)
+    const response = await axios.post('http://localhost:3200/Login', data);
+    console.log(response.data)
   }
   return (
     <form onSubmit={handleSubmit}>
@@ -22,11 +27,11 @@ function App(e) {
     <div className="App">
         <div className="input">
           <h2 className="title">Log In</h2>
-          <textarea className="userInput" type = {username} placeholder="Username" value= {username} onChange={(e) => setUsername(e.target.value)}></textarea>
+          <textarea className="userInput" type = {username} name="username" placeholder="Username" value= {username} onChange={(e) => setUsername(e.target.value)}></textarea>
           <h2></h2>
-          <textarea className="passinput" type = {password} placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}></textarea>
+          <textarea className="passinput" type = {password} name="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}></textarea>
           <h2></h2>
-          <button type= 'submit' className="login" onClick={(e) => handleSubmit(e)}>Log In</button>
+          <button type= 'submit' className="login" onClick={(e)=> handleSubmit(e)}>Log In</button>
         </div>
         {/* <GoogleSignIn /> */}
       </div></>
